dc3df0c177cedb127662cb87aa4cfee2a6c38aca
Nico Benz <nico-benz@gmx.net>
2023-09-18 14:25:44 +0200

    added calculation based on genres

--- calculate_keywords.py
+++ calculate_keywords.py
@@ -1,8 +1,10 @@
 import os
+import json
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+import statsmodels.api as sm
 
 
 def get_keywords(
@@ -13,8 +15,32 @@ def get_keywords(
     if limit_games > 0:
         clean_texts = clean_texts[0:limit_games]  # select only some texts for testing purposes
 
+    with open("data/genre_mapping.json", "r") as f:
+        mapping = json.loads(f.read())
+    genres = set(mapping.values())
+    texts_by_genre = {genre: [] for genre in genres}
+    for text in clean_texts:
+        text_genre = mapping[text]
+        texts_by_genre[text_genre].append(text)
+    texts_by_genre["All Genres"] = clean_texts
+
+    collected_texts = {}
+    lengths = []
+    for label, games in texts_by_genre.items():
+        if len(games) < 6:
+            pass
+        else:
+            collected_texts[label] = calculate_ngrams(games, num_ngrams, limit_ngrams, ngram_fraction, verbose)
+            lengths.append(len(games))
+            if verbose:
+                print("\n")
+
+    return collected_texts, lengths
+
+
+def calculate_ngrams(texts, num_ngrams, limit_ngrams, ngram_fraction, verbose=True):
     ngrams_collector = ["" for _ in range(num_ngrams)]
-    for text_idx, text in enumerate(clean_texts, start=1):
+    for text_idx, text in enumerate(texts, start=1):
         # open texts
         with open(f"data/transcripts/clean/{text}", "r") as f:
             filtered_tokens = f.read()
@@ -38,10 +64,7 @@ def get_keywords(
             ngrams_collector[i] += (' '.join(current_ngram) + " ")
             # print progress if needed
             if verbose:
-                print(f"\rGame {text_idx:03d}/{len(clean_texts):03d} | ngram {i+1:03d}/{num_ngrams:03d}", end="")
-    if verbose:
-        print("\n")
-
+                print(f"\rGame {text_idx:03d}/{len(texts):03d} | ngram {i + 1:03d}/{num_ngrams:03d}", end="")
     return ngrams_collector
 
 
@@ -76,31 +99,62 @@ def get_frequency_scores_from_ngrams(ngram_texts, num_lines, mode="count"):
     return score_collection
 
 
-def plot_keyword_relevance(freq_scores):
+def plot_keyword_relevance(freq_scores, numbers, save_label="", smoothing="avg"):
     sns.set_theme()
     sns.set_context("paper")
     custom_palette = sns.color_palette("pastel")
     sns.set_palette(custom_palette)
 
-    for idx, sublist in enumerate(freq_scores):
-        print(sublist)
+    normalized_freq_scores = []
+    for sublist in freq_scores:
+        sublist = np.array(sublist, dtype=float)
+        min_val = np.min(sublist)
+        max_val = np.max(sublist)
+        scaled_sublist = (sublist - min_val) / (max_val - min_val)
+        normalized_freq_scores.append(scaled_sublist)
+
+    for idx, sublist in enumerate(normalized_freq_scores):
         sublist = np.array(sublist, dtype=float)
         x = np.arange(len(sublist))
-        plt.plot(x, sublist, marker='', linestyle='-', linewidth=2, markersize=5, color=custom_palette[idx])
 
+        if smoothing == "avg":
+            smoothed_sublist = moving_average(sublist, window_size=5)
+        elif smoothing == "low":
+            smoothed_sublist = lowess_smoothing(sublist, frac=0.3)
+        else:
+            raise ValueError("Wrong smoothing mode. Please use 'avg' for Moving Average or 'low' for Lowess Smoothing.")
+
+        plt.plot(x[:len(smoothed_sublist)], smoothed_sublist, marker='', linestyle='-', linewidth=2, markersize=5,
+                 color=custom_palette[idx])
+
+    plt.title(f"Keyword Development for {save_label} (n={numbers})")
     plt.xlabel("N-Gram Index")
-    plt.ylabel("TF-IDF Scores")
+    plt.ylabel("Frequency Scores")
 
     plt.legend(title="Verbs", labels=action_verbs, loc="upper left")
 
     plt.tight_layout()
 
-    plt.savefig("data/results/plots/lineplot.pdf")
+    plt.savefig(f"data/results/plots/lineplot-{'_'.join(save_label.split(' '))}.pdf")
+    plt.clf()
+
+
+def moving_average(data, window_size=3):
+    weights = np.repeat(1.0, window_size) / window_size
+    smoothed_data = np.convolve(data, weights, 'valid')
+    return np.concatenate(([data[0]] * (window_size - 1), smoothed_data))
+
+
+def lowess_smoothing(data, frac=0.3):
+    # Apply Lowess smoothing to the data
+    lowess = sm.nonparametric.lowess(data, np.arange(len(data)), frac=frac)
+    return lowess[:, 1]
 
 
 # specify verbs (or tokens) to
-action_verbs = ["kill", "fight", "help", "talk", "bring"]
+action_verbs = ["kill", "help", "talk", "find"]  # bring?
 
-ngrams = get_keywords(10, 100, limit_ngrams=0, limit_games=0, verbose=False)
-scores = get_frequency_scores_from_ngrams(ngrams, len(action_verbs), mode="count")
-plot_keyword_relevance(scores)
+labeled_ngrams, num_texts = get_keywords(10, 200, limit_ngrams=0, limit_games=0, verbose=False)
+for (labeling, ngrams), num in zip(labeled_ngrams.items(), num_texts):
+    scores = get_frequency_scores_from_ngrams(ngrams, len(action_verbs), mode="count")
+    plot_keyword_relevance(scores, num, save_label=labeling, smoothing="low")
