af277ba72fc5e9c0214d092aad7aabaccad535c4
Nico Benz <nico-benz@gmx.net>
2023-09-18 10:41:25 +0200

    added a whitelist to exclude stopwords

--- clean_text.py
+++ clean_text.py
@@ -3,10 +3,17 @@ import re
 import spacy
 from tqdm import tqdm
 
+whitelist = []
+
 all_transcripts = [file for file in os.listdir("data/transcripts/raw") if ".DS_Store" not in file]
 all_transcripts.sort()
 
 nlp = spacy.load("en_core_web_sm")
+with open("data/custom_stopwords.text", "r") as f:
+    custom_stops = f.read()
+custom_stops = custom_stops.split("\n")
+custom_stops = [stop for stop in custom_stops if stop != ""]
+
 for transcript in tqdm(all_transcripts):
     with open(f"data/transcripts/raw/{transcript}") as f:
         content = f.read()
@@ -14,7 +21,10 @@ for transcript in tqdm(all_transcripts):
     script = content.replace('\n', ' ')
     script = re.sub(r'\s+', ' ', script)
     doc = nlp(script)
-    filtered_tokens = [tok.lemma_.lower() for tok in doc if not (tok.is_stop or tok.ent_type_ or not tok.is_alpha)]
+    filtered_tokens = [
+        tok.lemma_.lower()
+        for tok in doc
+        if not (tok.is_stop and tok.lemma_.lower() not in whitelist or tok.ent_type_ or not tok.is_alpha) and tok.lemma_.lower() not in custom_stops]
     filtered_tokens = ' '.join(filtered_tokens)
 
     with open(f"data/transcripts/clean/{transcript}", "w") as f:
